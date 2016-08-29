0a805a9fa0ec14fecfa8c977c1652217e09ffc8f
Andy--- <andreas.huber@b-tu.de>
2016-08-29 13:39:57 +0200

    Added load-if-exists function

--- init.el
+++ init.el
@@ -16,6 +16,11 @@
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (load custom-file)
 
+(defun load-if-exists (file)
+  "Load `file` if it exists."
+  (when (file-exists-p file)
+    (load file)))
+
 ;;octave mode
 ;;(autoload 'run-octave "octave-inf" nil t)
 ;;(autoload 'octave-mode "octave-mod" nil t)
