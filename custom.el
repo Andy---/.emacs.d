2f32445403c1f47f04af0ea3fc4160f29214b9d0
andy <andreas.huber@posteo.de>
2020-11-11 20:57:27 +0200

    some postgres connection and org-mode stuff in custom.el

--- custom.el
+++ custom.el
@@ -4,13 +4,19 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" "d8dc153c58354d612b2576fea87fe676a3a5d43bcc71170c62ddde4a1ad9e1fb" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" default)))
-
+   '("3d4df186126c347e002c8366d32016948068d2e9198c496093a96775cc3b3eaa" "d8dc153c58354d612b2576fea87fe676a3a5d43bcc71170c62ddde4a1ad9e1fb" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "f9574c9ede3f64d57b3aa9b9cef621d54e2e503f4d75d8613cbcc4ca1c962c21" default))
  '(inhibit-startup-screen t)
  '(js2-basic-offset 2)
+ '(org-adapt-indentation nil)
+ '(org-log-into-drawer t)
  '(package-selected-packages
-   '(js2-mode company-web yasnippet emmet-mode web-mode spacemacs-theme soundklaus abyss-theme ido-vertical-mode magit smex ess multi-term ebib elpy ein use-package)))
+   '(org js2-mode company-web yasnippet emmet-mode web-mode spacemacs-theme soundklaus abyss-theme ido-vertical-mode magit smex ess multi-term ebib elpy ein use-package))
+ '(sql-connection-alist
+   '(("datawarehouse1"
+      (sql-product 'postgres)
+      (sql-user "andy")
+      (sql-database "datawarehouse1")
+      (sql-server "localhost")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
