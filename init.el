4a7893e5f2aafeff50612cf28b0472af5e5d1663
Andy--- <andreas.huber@posteo.de>
2020-12-14 16:26:11 +0200

    add templates for org-babel
    -  emacs-lisp
    -  shell
    -  python

--- init.el
+++ init.el
@@ -79,6 +79,12 @@
     '((emacs-lisp . t)
       (python . t)))
 
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+(add-to-list 'org-structure-template-alist '("py" . "src python"))
+
 ;;SQL
 (setq sql-indent-offset 2)
 ;;Load database connection settings
